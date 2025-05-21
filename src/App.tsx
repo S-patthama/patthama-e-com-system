02f9ca9ec51316d68a13989db402df093e25e906
patthama <patthah@ntplc.co.th>
2025-05-21 15:15:08 +0700

    AEC-1 create a sign up ui form

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>E-com System 1.0.0 (add login page)(update shopping cart)</h1>
+      <h2>create a sign up ui form</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
